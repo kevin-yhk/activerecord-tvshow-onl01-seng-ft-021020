1085cf73986695a252c6f3563bf39d2b72aaa6b3
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-19 04:24:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,7 @@ class Show < ActiveRecord::Base
   def Show::most_popular_show
     Show.find_by("rating =?", Show::highest_rating)
   end 
+  
+  def Show::lowest_rating
+    Show.minimum(:rating)
 end 
